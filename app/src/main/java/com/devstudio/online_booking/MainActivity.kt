816c7237c7fbe9f21949df13957c50a3179a494c
kavin511 <kavinalmighty@gmail.com>
2022-06-08 12:43:02 +0530

    Initial Commit

---
+++ app/src/main/java/com/devstudio/online_booking/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.devstudio.online_booking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devstudio.online_booking.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
